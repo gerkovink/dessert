d50ee0211fe7231a09b49fdd679f783c1d04df9e
Gerko Vink <g.vink@uu.nl>
2022-10-28 15:27:58 +0200

    only test file existence if write access

--- tests/testthat/test-dessert.R
+++ tests/testthat/test-dessert.R
@@ -1,14 +1,20 @@
 # Create a linear model
 fit <- lm(dist ~ speed, data = cars)
+testthat::expect_true(class(fit) == "lm")
 # Test if runs
-testthat::expect_no_error(dessert(fit))
+testthat::expect_no_error(dessert(fit, output_format = "html_document"))
 # Test if storage location is communicated
-testthat::expect_output(dessert(fit))
-# Test if files are produced
-testthat::expect_true(file.exists("dessert_envir.RData"))
-testthat::expect_true(file.exists("lm.Rmd"))
-testthat::expect_true(file.exists("lm.docx"))
-testthat::expect_true(file.exists("lm.html"))
-testthat::expect_true(file.exists("lm.pdf"))
-# Remove produced files from test environment
-unlink(c("dessert_envir.RData", "lm.Rmd", "lm.docx", "lm.html", "lm.pdf"))
+testthat::expect_output(dessert(fit, output_format = "html_document"))
+# Check file access and if sufficient, run tests
+if (file.access(".", 2) == 0) {
+  # Test if files are produced
+  testthat::expect_true(file.exists("dessert_envir.RData"))
+  testthat::expect_true(file.exists("lm.Rmd"))
+  testthat::expect_true(file.exists("lm.html"))
+  # Remove produced files from test environment
+  unlink(c(
+    "dessert_envir.RData",
+    "lm.Rmd",
+    "lm.html"
+  ))
+}
