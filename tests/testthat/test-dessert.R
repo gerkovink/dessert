ee8238bc8d9ad96e0de2f4cb31d851adc0423de6
Gerko Vink <g.vink@uu.nl>
2022-10-28 15:41:47 +0200

    Update test-dessert.R

--- tests/testthat/test-dessert.R
+++ tests/testthat/test-dessert.R
@@ -1,14 +1,18 @@
 # Create a linear model
 fit <- lm(dist ~ speed, data = cars)
-# Test if runs
-testthat::expect_no_error(dessert(fit))
-# Test if storage location is communicated
-testthat::expect_output(dessert(fit))
-# Test if files are produced
-testthat::expect_true(file.exists("dessert_envir.RData"))
-testthat::expect_true(file.exists("lm.Rmd"))
-testthat::expect_true(file.exists("lm.docx"))
-testthat::expect_true(file.exists("lm.html"))
-testthat::expect_true(file.exists("lm.pdf"))
-# Remove produced files from test environment
-unlink(c("dessert_envir.RData", "lm.Rmd", "lm.docx", "lm.html", "lm.pdf"))
+# Check file access and if sufficient, run tests
+if (file.access(".", 2) == 0) {
+  # Test if runs
+  testthat::expect_no_error(dessert(fit, output_format = "html_document"))
+  # Test if storage location is communicated
+  testthat::expect_output(dessert(fit, output_format = "html_document"))
+
+  # Test if files are produced
+  testthat::expect_true(file.exists("dessert_envir.RData"))
+  testthat::expect_true(file.exists("lm.Rmd"))
+  testthat::expect_true(file.exists("lm.html"))
+  # Remove produced files from test environment
+  unlink(c("dessert_envir.RData",
+           "lm.Rmd",
+           "lm.html"))
+}
