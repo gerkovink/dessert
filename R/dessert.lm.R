d69dbf1d31768bbfe3fa355653e904e6be1d7769
Gerko Vink <g.vink@uu.nl>
2022-10-28 15:27:44 +0200

    only run html example

--- R/dessert.lm.R
+++ R/dessert.lm.R
@@ -10,22 +10,23 @@
 #' @examples
 #'
 #' # Call a lm dessert on a fitted linear model
+#'
 #' fit <- lm(dist ~ speed, data = cars)
-#' dessert.lm(fit)
+#' dessert.lm(fit, output_format = "html_document")
 #'
 #' # Remove the generated files in R-CMD-Check
 #' # Don't run as an example in real life
-#' unlink(c("dessert_envir.RData", "lm.Rmd", "lm.docx", "lm.html", "lm.pdf"))
-#'
+#' unlink(c("dessert_envir.RData", "lm.Rmd", "lm.html"))
 
 dessert.lm <- function(data,
-                    output_format = NULL,
-                    output_dir = NULL) {
+                       output_format = NULL,
+                       output_dir = NULL) {
   # rmd location
-  rmdloc <- paste(.libPaths(), "dessert", "rmd", "lm.Rmd", sep = "/")
+  rmdloc <-
+    paste(.libPaths(), "dessert", "rmd", "lm.Rmd", sep = "/")
 
   # get the directory of the file calling dessert
-  if (!rstudioapi::isAvailable()){
+  if (!rstudioapi::isAvailable()) {
     if (is.null(output_dir)) {
       output_dir <- getwd()
     }
@@ -41,34 +42,48 @@ dessert.lm <- function(data,
     }
   }
 
+  if (file.access(output_dir, 2) != 0) {
+    cat(
+      paste(
+        "No write access to the output directory: ",
+        output_dir,
+        ". Please specify an output directory with sufficient access privileges.",
+        sep = ""
+      )
+    )
+  } else {
+    # copy the .Rmd file to the output location
+    file.copy(
+      from = rmdloc,
+      to = output_dir,
+      overwrite = TRUE,
+      recursive = TRUE,
+      copy.mode = TRUE
+    )
 
+    # default output is html, pdf, word, setting output format to all
+    if (is.null(output_format)) {
+      rmarkdown::render(
+        input = paste(output_dir, "lm.rmd", sep = "/"),
+        output_format = "all",
+        output_dir = output_dir,
+        quiet = TRUE
+      )
+    }
+    # user specified output format
+    else {
+      rmarkdown::render(
+        input = paste(output_dir, "lm.rmd", sep = "/"),
+        output_format = output_format,
+        output_dir = output_dir,
+        quiet = TRUE
+      )
+    }
 
-  # copy the .Rmd file to the output location
-  file.copy(from = rmdloc,
-            to = output_dir,
-            overwrite = TRUE,
-            copy.mode = TRUE)
+    # store the data as RData
+    save(data, file = paste(output_dir, "dessert_envir.RData", sep = "/"))
 
-  # default output is html, pdf, word, setting output format to all
-  if (is.null(output_format)) {
-    rmarkdown::render(
-      input = paste(output_dir, "lm.rmd", sep = "/"),
-      output_format = "all",
-      output_dir = output_dir,
-      quiet = TRUE)
+    # print statement
+    cat("files have been stored in", output_dir)
   }
-  # user specified output format
-  else {
-    rmarkdown::render(
-      input = paste(output_dir, "lm.rmd", sep = "/"),
-      output_format = output_format,
-      output_dir = output_dir,
-      quiet = TRUE)
-  }
-
-  # store the data as RData
-  save(data, file = paste(output_dir, "dessert_envir.RData", sep = "/"))
-
-  # print statement
-  cat("files have been stored in", output_dir)
 }
